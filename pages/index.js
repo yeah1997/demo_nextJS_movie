c9b0cfd54f2d0508f7ea13a5044dc4614deb4d7b
wangjian <wjb19891223@163.com>
2020-07-18 14:13:29 +0800

    实现导航组件布局

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,11 @@
-import Header from '../components/Header';
+import Header from "../components/Header";
+import Navigation from "../components/Navigation";
 
 export default function Home() {
-  return <Header />
+  return (
+    <>
+      <Header />
+      <Navigation />
+    </>
+  );
 }
